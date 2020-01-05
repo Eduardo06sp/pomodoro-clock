cef42312545edf09c888e02e9e7621b2a6a9703a
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-01-05 02:05:53 -0800

    Fix incorrect method

--- js/app.js
+++ js/app.js
@@ -1,11 +1,8 @@
 /** COUNT DOWN 25 MINUTES **/
-const min = document.getElementByClass('min');
-const sec = document.getElementByClass('sec');
-  console.log(sec);
-  console.log('test');
+const min = document.getElementsByClassName('min');
+const sec = document.getElementsByClassName('sec');
 
 const secTimer = setInterval(countSecDown, 1000);
 function countSecDown() {
   sec -= 1;
-  console.log(sec);
 }
