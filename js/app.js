2a66b4dbd6a40d63953156fc84e5c9bb991ba0e0
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2020-01-05 18:01:27 -0800

    Add sec & min count down logic

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 /** COUNT DOWN 25 MINUTES **/
-const min = document.getElementsByClassName('min');
-const sec = document.getElementsByClassName('sec');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
 
 const secTimer = setInterval(countSecDown, 1000);
+
 function countSecDown() {
-  sec -= 1;
+  if (sec.textContent <= 0) {
+    sec.textContent = 60;
+    min.textContent -= 1;
+  }
+
+  sec.textContent -= 1;
 }
